8a04a74a948596f383fb362919c4dfedf47741c4
Justin Meyer <justin.meyer@starz.com>
2016-08-18 08:07:27 -0600

    sidenav working wo css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 import { MdCardModule } from '@angular2-material/card';
 import { MdButtonModule } from '@angular2-material/button';
 
-
+import { RedirectManagerComponent } from './redirect-manager/redirect-manager.component';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [RedirectManagerComponent
+  ]
 })
 export class AppComponent {
   title = 'jtfc!';
